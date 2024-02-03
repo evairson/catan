5ce4c745169150e89ae3587163610f5cb4671f42
Grimmins <lukas.koltes@hotmail.fr>
2024-02-03 11:35:11 +0100

    - ajout feature pour lancer la frame sur second screen

--- GameWindow.java
+++ GameWindow.java
@@ -11,17 +11,22 @@ public class GameWindow{
         GraphicsDevice gd = ge.getDefaultScreenDevice();
         //get la taille de ton écran
 
-
         jframe = new JFrame("JSP ENCORE MDR");
         gd.setFullScreenWindow(jframe);
+
+        GraphicsDevice[] gds = ge.getScreenDevices();
+        if(gds.length > 1) { // S'il y a plus d'un écran, ça lance sur le deuxième écran
+            Rectangle bounds = gds[0].getDefaultConfiguration().getBounds(); // Récupère les limites du deuxième écran
+            jframe.setLocation(bounds.x + (bounds.width - jframe.getWidth()) / 2, bounds.y + (bounds.height - jframe.getHeight()) / 2);
+        } else {
+            jframe.setLocationRelativeTo(null);
+        } //full useless si un écran :)
 
         Constants.Game.WIDTH = jframe.getWidth();
         Constants.Game.HEIGHT = jframe.getHeight();
 
 
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
         jframe.add(gamePanel);
         jframe.setVisible(true);
 
