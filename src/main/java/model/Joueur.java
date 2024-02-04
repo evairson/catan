2ca2183c4740a3578238deb290ca05c0bc0f8f46
evairson <eva.herson@me.com>
2024-02-04 19:37:41 +0100

    #9 create joueur class and add attributs

---
+++ src/main/java/model/Joueur.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.lang.Math;  
+
+public class Joueur {
+
+    public enum Color {
+        RED,
+        WHITE,
+        BLUE,
+        ORANGE
+    }
+
+    private Color color;
+    private Boolean turn;
+    private int dice1;
+    private int dice2;
+    // private ArrayList<Card> cardsRessources;
+    // private ArrayList<Card> cardsDev;
+    // private ArrayList<Building> buildings;
+
+// Getter / Setter :  ---------------
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color c){
+        color = c;
+    }
+
+    public Boolean isTurn(){
+        return turn;
+    }
+
+    public void setTurn(Boolean b){
+        turn = b;
+    }
+
+    public int getDies(){
+        return dice1 + dice2;
+    }
+
+// ------------------------------------
+
+    public void throwDice1(){
+        dice1 = (int)(Math.random()*7); // (max-min+1)*min
+    }
+
+    public void throwDice2(){
+        dice2 = (int)(Math.random()*7);
+    }
+
+    public void throwDices(){
+        throwDice1();
+        throwDice2();
+    }
+
+
+
+}
