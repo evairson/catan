01c56b0764ee36909ca409cb9ed164760c4a782c
Léo Vernicos <leo.vernicos@etu.u-paris.fr>
2024-02-09 00:06:31 +0100

    au bout du rouleau la

--- src/main/java/model/geometry/CubeCoordinates.java
+++ src/main/java/model/geometry/CubeCoordinates.java
@@ -61,7 +61,7 @@ public class CubeCoordinates {
         if (direction < 0 || direction > 5) {
             throw new IllegalArgumentException("Direction must be between 0 and 5");
         }
-        return Constants.CubeCoordinatesConst.DIRECTIONS.get(direction);
+        return Constants.CubeCoordinatesConst.DIRECTIONS[direction];
     }
 
     public CubeCoordinates neighbor(int direction/* 0 to 5 */) {
