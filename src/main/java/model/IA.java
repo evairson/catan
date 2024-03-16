065e75ef1648ebf9e621fd8792ba848e237321c7
Lucas Verriere <lucasverriere92600@gmail.com>
2024-03-16 10:34:08 +0100

    creation de la classe rien de ouf

---
+++ src/main/java/model/IA.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.HashMap;
+
+import model.geometry.Point;
+import model.tiles.TileVertex;
+
+public class IA {
+
+    public IA() {
+
+    }
+
+    public void vertexMapdToValue(HashMap<Point, TileVertex> verticesMap) {
+        
+    }
+
+}
