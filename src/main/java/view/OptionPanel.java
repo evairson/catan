793ac1d2e8dc7151577a70c251bab8e3118751cb
Lucas Verriere <lucasverriere92600@gmail.com>
2024-05-09 18:23:44 +0200

    placement des boutons et images dans les options

--- src/main/java/view/OptionPanel.java
+++ src/main/java/view/OptionPanel.java
@@ -6,16 +6,19 @@ import javax.swing.event.ChangeListener;
 
 import java.awt.CardLayout;
 import java.awt.Container;
+import java.awt.Rectangle;
 
 import javax.swing.*;
 
 import others.Music;
 import view.utilities.ButtonImage;
+import view.utilities.Resolution;
 
 public class OptionPanel extends JPanel {
     private final String basePath = "src/main/resources/";
 
     public OptionPanel() {
+        setLayout(null);
         JSlider volumeSlider = new JSlider(-80, 6);
         setVisible(true);
         volumeSlider.addChangeListener(new ChangeListener() {
@@ -25,6 +28,9 @@ public class OptionPanel extends JPanel {
                 Music.setVolume(volumeSlider.getValue());
             }
         });
+        int[] coords = Resolution.calculateResolution(550, 100);
+        volumeSlider.setBounds(coords[0], coords[1], 200, 100);
+        volumeSlider.setLocation(coords[0], coords[1]);
         add(volumeSlider);
 
         JButton quitBtn = new ButtonImage(basePath + "quitOption.png", basePath + "quitOption.png",
@@ -36,7 +42,6 @@ public class OptionPanel extends JPanel {
         Container parent = getParent();
         CardLayout parentLayout = (CardLayout) parent.getLayout();
         parentLayout.show(parent, "mainMenu");
-        System.out.println("caca");
         return;
     }
 }
