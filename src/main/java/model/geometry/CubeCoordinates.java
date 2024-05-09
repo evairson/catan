17472538953a6cb329cf1321b3ac3d557bfd2886
vernicos <leo.vernicos@etu.u-paris.fr>
2024-05-09 04:45:58 +0200

    index on dev: 1d4eb52 Merge branch 'iss71-IA' into 'dev'

--- src/main/java/model/geometry/CubeCoordinates.java
+++ src/main/java/model/geometry/CubeCoordinates.java
@@ -4,11 +4,34 @@ import java.io.Serializable;
 
 import others.Constants;
 
+/**
+ * CubeCoordinates
+ *This class represents the cube coordinates of a hexagon in a hexagonal grid.
+    *The cube coordinates are a 3D coordinate system where each hexagon is represented by 3 coordinates (q, r, s).
+    *The conventions are q for the x-axis, r for the y-axis, and s for the z-axis.
+    *The sum of the three coordinates must be 0.
+    *The cube coordinates are used to represent the position of a hexagon in a hexagonal grid.
+    *The cube coordinates are used to:
+    *   calculate the distance between two hexagons in a hexagonal grid.
+    *   calculate the direction of a hexagon in a hexagonal grid.
+    *   calculate the neighbor of a hexagon in a hexagonal grid.
+    *   calculate the length of a hexagon in a hexagonal grid.
+    *   calculate the addition, subtraction, and multiplication of two cube coordinates.
+    *   calculate the equality of two cube coordinates.
+    */
 public class CubeCoordinates implements Serializable {
     private int q;
     private int r;
     private int s;
-
+    
+    /**
+     * Constructor
+     * @param q
+     * @param r
+     * @param s
+     * @throws IllegalArgumentException if q + r + s != 0
+     * @return CubeCoordinates with the given coordinates
+     */
     public CubeCoordinates(int q, int r, int s) {
         if (q + r + s != 0) {
             throw new IllegalArgumentException("q + r + s must be 0");
@@ -18,6 +41,10 @@ public class CubeCoordinates implements Serializable {
         this.s = s;
     }
 
+    /**
+     * @param obj, 
+     * @return boolean true if the given object is equal to this cube coordinates, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == this) {
@@ -30,6 +57,10 @@ public class CubeCoordinates implements Serializable {
         return (c.q == this.q && c.r == this.r && c.s == this.s);
     }
 
+
+    /**
+     * @return int the hash code of this cube coordinates
+     */
     @Override
     public int hashCode() {
         int result = 17;
@@ -38,33 +69,61 @@ public class CubeCoordinates implements Serializable {
         result = 31 * result + s;
         return result;
     }
-
+    /**
+     * @param b
+     * @return CubeCoordinates the addition of this cube coordinates and the given cube coordinates
+     */
     public CubeCoordinates add(CubeCoordinates b) {
         return new CubeCoordinates(this.q + b.q, this.r + b.r, this.s + b.s);
     }
 
+    /**
+     * @param b
+     * @return CubeCoordinates the subtraction of this cube coordinates and the given cube coordinates
+     */
     public CubeCoordinates subtract(CubeCoordinates b) {
         return new CubeCoordinates(this.q - b.q, this.r - b.r, this.s - b.s);
     }
 
+    /**
+     * @param k
+     * @return CubeCoordinates the multiplication of this cube coordinates by the given number
+     */
     public CubeCoordinates multiply(int k) {
         return new CubeCoordinates(this.q * k, this.r * k, this.s * k);
     }
 
+    /**
+     * @return int the length of this cube coordinates
+     */
     public int length() {
         return (Math.abs(this.q) + Math.abs(this.r) + Math.abs(this.s)) / 2;
     }
 
+    /**
+     * @param b
+     * @return int the distance between this cube coordinates and the given cube coordinates
+     */
     public int distance(CubeCoordinates b) {
         return this.subtract(b).length();
     }
 
+    /**
+     * @param direction
+     * 
+     * The directions represent the 6 directions of a hexagon in a hexagonal grid.
+     * The directions are represented by cube coordinates.
+     * The directions are used to calculate the neighbor of a hexagon in a hexagonal grid.
+     * 
+     * @return CubeCoordinates the direction of this cube coordinates in the given direction
+     */
     public CubeCoordinates direction(int direction/* 0 to 5 */) {
         if (direction < 0 || direction > 5) {
             throw new IllegalArgumentException("Direction must be between 0 and 5");
         }
         return Constants.CubeCoordinatesConst.DIRECTIONS[direction];
     }
+
 
     public CubeCoordinates neighbor(int direction/* 0 to 5 */) {
         if (direction < 0 || direction > 5) {
