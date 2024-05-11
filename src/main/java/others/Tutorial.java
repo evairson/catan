31d7e5066940bf3d5a3c56ab6ae6eae21fcc77c2
Lucas Verriere <lucasverriere92600@gmail.com>
2024-05-11 03:48:00 +0200

    début du texte

--- src/main/java/others/Tutorial.java
+++ src/main/java/others/Tutorial.java
@@ -10,6 +10,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import view.utilities.ButtonImage;
 import view.utilities.Resolution;
@@ -18,6 +19,29 @@ public class Tutorial extends JPanel {
     private Image backgroundImage;
     private int i = 1;
     private String basePath = "src/main/resources/";
+    private Image[] images = new Image[2];
+    private JLabel texte;
+    private String[] textes =
+        {"<html> Le jeu se présente de cette manière, les noms des joueurs sont en haut de l'écran. </br>"
+            + "Le joueur dont c'est le tour est souligner. </br>"
+            + "Sur la gauche il y a le plateau de jeu et sur la droite un chat avec les autres joueurs"
+            + "Nous verrons le reste du menu plus tard. </br> Appuyer sur suivant pour continuer</html>",
+        "Le jeu commence par le positionnement de deux colonie et d'une route collée à ces colonies.",
+        "Le joueur suivant place sa colinie etc...\n"
+            + "Une fois arrivé au dernier joueur cela recommence mais cette fois en partant de la fin",
+        "C'est donc au tour du joueur 1 de placé sa colonie",
+        "Une fois le placement des colonies de départ la partie peut commencer."
+            + "Il faut lancer les dés",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15"};
 
     public Tutorial() {
         setLayout(null);
@@ -36,18 +60,27 @@ public class Tutorial extends JPanel {
                 850, 60, 0.5, this::previous, null);
         add(prevBtn);
 
-        JLabel texte = new JLabel("bonjour j'adore les fruits");
-        int[] coords = Resolution.calculateResolution(300, 475);
+        texte = new JLabel(textes[0]);
+        int[] coords = Resolution.calculateResolution(450, 450);
         texte.setLocation(coords[0], coords[1]);
         texte.setBounds(coords[0], coords[1], 600, 200);
         texte.setBackground(Color.GRAY);
         texte.setOpaque(true);
+        texte.setHorizontalAlignment(SwingConstants.CENTER); // Aligner le texte au centre
+        texte.setVerticalAlignment(SwingConstants.TOP); // Aligner le texte en haut
+        texte.setVerticalTextPosition(SwingConstants.TOP); // Positionner le texte en haut
+        texte.setHorizontalTextPosition(SwingConstants.CENTER);
 
-
+        ImageIcon iconSheep = new ImageIcon(basePath + "tutoriel/sheep.png");
         ImageIcon icon = new ImageIcon(basePath + "tutoriel/1.png");
+        ImageIcon iconBubble = new ImageIcon(basePath + "tutoriel/speechbubble.png");
+
         int width = Constants.Game.WIDTH;
         int height = Constants.Game.HEIGHT;
         backgroundImage = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        images[0] = iconSheep.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        images[1] = iconBubble.getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+
         add(texte);
     }
 
@@ -64,6 +97,7 @@ public class Tutorial extends JPanel {
             backToMenu();
         }
         updateImage();
+        updateText();
     }
 
     public void previous() {
@@ -73,6 +107,11 @@ public class Tutorial extends JPanel {
             backToMenu();
         }
         updateImage();
+        updateText();
+    }
+
+    public void updateText() {
+        texte.setText(textes[i-1]);
     }
 
     public void updateImage() {
@@ -86,5 +125,7 @@ public class Tutorial extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImage, 0, 0, this);
+        g.drawImage(images[0], 185, 700, null);
+        //g.drawImage(images[1], 500, 300, null);
     }
 }
