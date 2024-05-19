c40ad193a9a26b0946555fce6028f713f962ca53
AmaelMSC <amael.mangeantsastre@gmail.com>
2024-05-19 19:03:57 +0200

    Updated launch.sh

--- launch.sh
+++ launch.sh
@@ -14,28 +14,42 @@ fi
 
 
 echo "Starting..."
-find -name "*.java" > files.txt
-sed -i "/\b\(test\|Test\)\b/d" files.txt
-echo "Compiling..."
-javac  -d src/classFiles @files.txt
-
 if [ $platform -eq 0 ] 
 then
-    CLASSPATH=".;src/classFiles/;src/classFiles/*;src/classFiles/*/*;src/classFiles/*/*/*;src/classFiles/*/*/*/*;src/classFiles/*/*/*/*/*;src/main/resources"
+  find -name "*.java" > files.txt
+  sed -i "/\b\(test\|Test\)\b/d" files.txt
+  echo "Compiling..."
+  javac  -d src/classFiles @files.txt
+  CLASSPATH=".;src/classFiles/;src/classFiles/*;src/classFiles/*/*;src/classFiles/*/*/*;src/classFiles/*/*/*/*;src/classFiles/*/*/*/*/*;src/main/resources"
+  echo "Running..."
+  java -cp $CLASSPATH start.Main
+
+  echo "Cleaning up..."
+  DELCLASSPATH="src/classFiles/*"
+  rm -r $DELCLASSPATH
+  rm files.txt
+  #For Windows
+  #read -p "Press any key to continue" x
 elif [ $platform -eq 1 ] 
-then 
-    CLASSPATH=".:src/classFiles/:src/classFiles/*:src/classFiles/*/*:src/classFiles/*/*/*:src/classFiles/*/*/*/*:src/classFiles/*/*/*/*/*:src/main/resources"
+then
+  # Répertoire de sortie des classes compilées
+  OUTPUT_DIR="bin"
+  SRC_DIR="src/main/java"
+  RESOURCES_DIR="src/main/resources"
+  LIB_DIR="lib"
+
+  # Trouver et compiler tous les fichiers Java
+  echo "Compiling Java sources..."
+  find $SRC_DIR -name "*.java" | xargs javac -classpath "$LIB_DIR/*" -d $OUTPUT_DIR
+
+  # Copier les ressources dans le répertoire de sortie
+  echo "Copying resources..."
+  cp -r $RESOURCES_DIR/* $OUTPUT_DIR
+
+  # Exécuter l'application
+  echo "Running the application..."
+  java -classpath "$OUTPUT_DIR:$LIB_DIR/*" start.Main
 else 
     echo "Please enter 0 (Windows start) or 1 (Linux start)"
     exit 1
 fi
-
-echo "Running..."
-java -cp $CLASSPATH start.Main
-
-echo "Cleaning up..."
-DELCLASSPATH="src/classFiles/*"
-rm -r $DELCLASSPATH
-rm files.txt
-#For Windows
-#read -p "Press any key to continue" x
