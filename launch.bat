2546621aa094ac6c48122387527ed2d71bacf2f7
AmaelMSC <amael.mangeantsastre@gmail.com>
2024-05-19 19:07:51 +0200

    Updated launch.bat

--- launch.bat
+++ launch.bat
@@ -1,49 +1,23 @@
 @echo off
-setlocal enabledelayedexpansion
 
-:: This is a simple Batch script to compile all files and run the Main file.
+REM Trouver tous les fichiers .java et les lister dans files.txt
+dir /S /B *.java > files.txt
 
-:: How to run the program
-:: Path: launch.bat
+REM Supprimer les lignes contenant 'test' ou 'Test'
+powershell -Command "(Get-Content files.txt) -notmatch '\b(test|Test)\b' | Set-Content files.txt"
 
-echo Which platform are you on ?
-echo (0) - Windows
-echo (1) - Linux
-set /p platform=Enter platform (0 for Windows, 1 for Linux): 
-
-if not "%platform%"=="0" if not "%platform%"=="1" (
-    echo "Error: Invalid argument supplied. Should be 0 (Windows) or 1 (Linux)"
-    pause
-    exit /b 1
-)
-
-::find all java file paths and put them in a file
-echo Starting...
-dir /s /b *.java > files.txt
-:: Compile the program
 echo Compiling...
-javac -d src/classFiles @files.txt
+REM Compiler les fichiers .java listés dans files.txt et placer les .class dans src\classFiles
+javac -d src\classFiles @files.txt
 
-:: Set the classpath
-if "%platform%"=="0" (
-    set "CLASSPATH=.;src/classFiles/;src/classFiles/*;src/classFiles/*/*;src/classFiles/*/*/*;src/classFiles/*/*/*/*"
-) else if "%platform%"=="1" (
-    set "CLASSPATH=.:src/classFiles/:src/classFiles/*:src/classFiles/*/*:src/classFiles/*/*/*:src/classFiles/*/*/*/*"
-) else (
-    echo "Please enter 0 (Windows start) or 1 (Linux start)"
-    pause
-    exit /b 1
-)
+REM Définir le CLASSPATH
+set CLASSPATH=.;src\classFiles\;src\classFiles\*;src\classFiles\*\*;src\classFiles\*\*\*;src\classFiles\*\*\*\*;src\classFiles\*\*\*\*\*;src\main\resources
 
-:: Run the program
-echo Running... 
+echo Running...
+REM Exécuter la classe principale start.Main
 java -cp %CLASSPATH% start.Main
-echo Cleaning up...
 
-:: Clean up
 echo Cleaning up...
-set DELCLASSPATH="src\classFiles\*"
-del /q /s !DELCLASSPATH! >nul
-del files.txt >nul
-pause
-exit /b 0
+REM Supprimer les fichiers compilés et files.txt
+rmdir /S /Q src\classFiles
+del files.txt
